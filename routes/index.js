a56d40776c81a481f67a664174e92a59f4a4c6b1
angel-pm <angel.pime.99@gmail.com>
2022-10-04 20:31:52 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const gods = require('./gods');
+const users = require('./users');
+
+router.get('/', (req, res) => {
+    res.json({'info': 'Welcome to gods API!'})
+});
+
+router.use('/gods', gods);
+router.use('/users', users);
+
+
+module.exports = router;
